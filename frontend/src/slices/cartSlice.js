6bbddeddd252561176bd66fef0ef1578bbe325c0
Chaitya Vohera <vohera.c@northeastern.edu>
2023-12-05 16:47:17 -0500

    test
    test

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,39 +1,49 @@
-import {createSlice} from "@reduxjs/toolkit";
-import { updateCart } from "../utils/cartUtilis";
-const initialState= localStorage.getItem("cart") 
-? JSON.parse(localStorage.getItem('cart')) : {cartItems: []};
+import { createSlice } from '@reduxjs/toolkit';
+import { updateCart } from '../utils/cartUtils';
 
-
+const initialState = localStorage.getItem('cart')
+  ? JSON.parse(localStorage.getItem('cart'))
+  : { cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal' };
 
 const cartSlice = createSlice({
-    name:'cart',
-    initialState,
-    reducers:{
-        addToCart : (state, action) =>{
-            const item = action.payload;
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      // The item to add to the cart
+      const item = action.payload;
 
-            const existItem = state.cartItems.find((x)=> x._id === item._id);
+      // Check if the item is already in the cart
+      const existItem = state.cartItems.find((x) => x._id === item._id);
 
-            if(existItem){
-                state.cartItems = state.cartItems.map((x)=>
-                x._id === existItem._id ? item : x
-                );
-            }
-            else{
-                state.cartItems = [...state.cartItems, item];
-            } 
-            return updateCart(state);
-        },
-        removeFromCart: (state, action) => {
-            state.cartItems = state.cartItems.filter((x)=> x._id !== action.payload);
-            
-            return updateCart(state);
+      if (existItem) {
+        // If exists, update quantity
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
+      } else {
+        // If not exists, add new item to cartItems
+        state.cartItems = [...state.cartItems, item];
+      }
 
-        }
+      // Update the prices and save to storage
+      return updateCart(state, item);
     },
+    removeFromCart: (state, action) => {
+      // Filter out the item to remove from the cart
+      state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
 
+      // Update the prices and save to storage
+      return updateCart(state);
+    },
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload;
+      localStorage.setItem('cart', JSON.stringify(state));
+    },
+  },
 });
 
-export const {addToCart, removeFromCart} = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress } =
+  cartSlice.actions;
 
-export default cartSlice.reducer;
+export default cartSlice.reducer; 
