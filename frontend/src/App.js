05d5179aec7f9bacebbc9586a27c285da2afbd06
Nitesh-NEU <more.ni@northeastern.edu>
2023-12-06 12:50:53 -0500

    Toast issue solved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,8 @@ import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
-import { ToastContainer } from "react-bootstrap";
-import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { logout } from './slices/authSlice';
 
 const App = () => {
@@ -33,7 +33,6 @@ const App = () => {
         pauseOnFocusLoss
         draggable
         pauseOnHover
-        theme="light"
       />
       {/* Same as */}
       <ToastContainer />
